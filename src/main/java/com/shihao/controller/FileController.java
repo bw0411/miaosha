8ea867be0f6ac187cbb4e85eb84af27738ac5b81
zhangshc <zhangshc@yonyou.com>
2019-02-18 15:18:05 +0800

    fastdfs

---
+++ src/main/java/com/shihao/controller/FileController.java
@@ -0,0 +1,15 @@
+package com.shihao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author: zhangshc
+ * @Date: 2019/2/18 14:15
+ * @Version 1.0
+ */
+@Controller
+@RequestMapping(value = "/file")
+public class FileController {
+
+}
